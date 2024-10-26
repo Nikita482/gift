af9ce3a599131b738e82ce33bcb6a77803c8baf1
Никита Кутилов <nikitakutilov@MacBook-Air-Nikita.local>
2024-10-26 17:17:41 +0300

    test2 commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
     <>
       <div>
         <h1>hq</h1>
+        <h1>test2</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
